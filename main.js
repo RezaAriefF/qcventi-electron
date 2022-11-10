1c110d7a55067f82e092303d97aff46fd43a3dd8
RezaAriefF <rezola4@gmail.com>
2022-11-10 14:51:46 +0700

    penambahan fitur exit 2

--- main.js
+++ main.js
@@ -38,6 +38,11 @@ const createWindow = () => {
 });
 
 };
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.

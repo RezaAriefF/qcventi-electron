6592fd2149c4fc20ba4969d1c5870149147593bd
ahmdxhabibi <ahmdxhabibi@gmail.com>
2022-11-29 09:59:50 +0700

    revisi container

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 // const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const { app, BrowserWindow, dialog } = require("electron");
 const createWindow = () => {
-
-const win = new BrowserWindow({
-    // resizable: false,
+  const win = new BrowserWindow({
+    resizable: false,
     // width: 1440,
     // height: 1006,
     show: false,
@@ -14,44 +13,40 @@ const win = new BrowserWindow({
     },
   });
 
-win.loadFile('index.html')
+  win.loadFile("index.html");
 
-var splash = new BrowserWindow({
-    // width: 1440, 
-    // height: 1006, 
+  var splash = new BrowserWindow({
+    // width: 1440,
+    // height: 1006,
     fullscreen: true,
-    transparent: true, 
-    frame: false, 
-    alwaysOnTop: true 
-});
-
-splash.loadFile('splashscreen.html');
-splash.center();
-setTimeout(function () {
-  splash.close();
-  win.center();
-  win.show();
-}, 4000);
-
-
-win.on("close", function (e) {
-  // import dialog terlebih dahulu pada module electron
-  const choice = dialog.showMessageBoxSync(this, {
-    type: "question",
-    buttons: ["Yes", "No"],
-    title: "Confirm",
-    message: "Are you sure you want to quit?",
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true,
   });
-  if (choice === 1) {
-    e.preventDefault();
-  }
-});
-win.maximize();
-win.loadFile("index.html");
 
+  splash.loadFile("splashscreen.html");
+  splash.center();
+  setTimeout(function () {
+    splash.close();
+    win.center();
+    win.show();
+  }, 4000);
 
+  win.on("close", function (e) {
+    // import dialog terlebih dahulu pada module electron
+    const choice = dialog.showMessageBoxSync(this, {
+      type: "question",
+      buttons: ["Yes", "No"],
+      title: "Confirm",
+      message: "Are you sure you want to quit?",
+    });
+    if (choice === 1) {
+      e.preventDefault();
+    }
+  });
+  win.maximize();
+  win.loadFile("index.html");
 };
-
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.

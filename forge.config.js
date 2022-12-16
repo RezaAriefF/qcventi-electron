a0470f090b5f47d061a6ee43b7e7c30c72070da0
RezaAriefF <rezola4@gmail.com>
2022-12-16 22:04:06 +0700

    fix icon

--- forge.config.js
+++ forge.config.js
@@ -1,21 +1,30 @@
 module.exports = {
   packagerConfig: {
-    icon: 'build/icon',
-    DisplayIcon: "build/icon",
-    installerIcon: "build/icon",
-    uninstallerIcon: "build/icon",
-    uninstallDisplayName: "QCVenti-Uninstaller 1.00",
-    oneClick: false,
-    allowToChangeInstallationDirectory: true
+    icon: 'build/iconqc', // no file extension required
+
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
+      config: {
+        // An URL to an ICO file to use as the application icon (displayed in Control Panel > Programs and Features).
+        icon: 'build/iconqc.ico',
+        // The ICO file to use as the icon for the generated Setup.exe
+        setupIcon: 'build/iconqc.ico',
+
+      },
       
     },
     {
       name: '@electron-forge/maker-zip',
+      config: {
+        // An URL to an ICO file to use as the application icon (displayed in Control Panel > Programs and Features).
+        icon: 'build/iconqc.ico',
+        // The ICO file to use as the icon for the generated Setup.exe
+        setupIcon: 'build/iconqc.ico',
+
+      },
       
     },
     {
